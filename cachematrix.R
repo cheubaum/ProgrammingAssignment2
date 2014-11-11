0d37933c7ae05fb1c567809f13856d394ff752cf
cheubaum <cheubaum@yahoo.com>
2014-11-11 06:33:29 -0600

    Updates to ProgammingAssigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+#############
+##
+## cachematrix.R: A pair of functions that cache the inverse of a matrix.
+##
+##              
+#############
 
-## Write a short comment describing this function
+##      inv_mat <<- inverse of the original matrix
+##      org_mat <<- copy of the original matrix
+
+##      makeCacheMatrix: This function creates a special "matrix" object 
+##                       that can cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Initialize the "holder" for a copy of the original x matrix
+        ## as well as the inverse of x
+        inv_mat <<- NULL 
+        org_mat <<- x
+        
+        ## If x is a valid matrix argument
+        if (is.matrix(x)) {                
+                ## Calculate the invese of x and save the results in inv_mat
+                inv_mat <<- solve(x)
+        }
+        
+        ## return the matrix that was passed in
+        x
 }
 
+##      cacheSolve:     This function computes the inverse of the special 
+##                      "matrix" returned by makeCacheMatrix above. If the 
+##                      inverse has already been calculated (and the matrix 
+##                      has not changed), then cacheSolve should retrieve the 
+##                      inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        
+        
+        ## check to see that ...
+        ##    x is a valid matrix
+        ##    copy of the original matrix is a valid matrix
+        ##    x and original matrix have the same dimention
+        ##    x and original matrix are equivalent 
+        ##    the cached inverse of x is set
+        ## 
+        ##  if all these are true, return the cached inverse of x
+        if (is.matrix(x) && 
+            is.matrix(org_mat) && 
+            dim(x) == dim(org_mat) && 
+            all(x == org_mat) &&
+            is.matrix(inv_mat)) { 
+                return (inv_mat)
+        }
+        
+        
+        ## If we get here, either the cache is not set or x has changed.
+        ## In either case, re-calculate the cache
+        makeCacheMatrix(x)
+        
+        ## Return the cached inverse of x
+        inv_mat
 }
